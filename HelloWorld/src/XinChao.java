8b34fea3a49f3a8d3fe4cb16b228e30ec5c2b420
Mai-Suong <suongcudai26@gmail.com>
2026-02-06 15:21:22 +0700

    HelloWorld
    Giới thiệu

---
+++ HelloWorld/src/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Xin chao the gioi Java!");
+		System.out.println("Chao mung den voi lap trinh Java");
+		System.out.println("Sam Thi Mai Suong - 66CNTT_2");
+	}
+
+}

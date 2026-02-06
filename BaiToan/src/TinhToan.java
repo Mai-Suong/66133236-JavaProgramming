0c9564402d38695ef7ab42a54a54ef7a210129bd
Mai-Suong <suongcudai26@gmail.com>
2026-02-06 15:45:08 +0700

    Bai1_3Tinh

---
+++ BaiToan/src/TinhToan.java
@@ -0,0 +1,10 @@
+public class TinhToan {
+
+	public static void main(String[] args) {
+		System.out.println("Tong cua 10 + 2 = " + (10+2));
+		System.out.println("Hieu cua 10 - 2 = " + (10-2));
+		System.out.println("Tich cua 10 x 2 = " + (10*2));
+		System.out.println("Thuong cua 10 : 2 = " + (10/2));
+	}
+
+}

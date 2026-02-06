780787afc5f6500518f85136a8d022a3ca5f22a7
Mai-Suong <suongcudai26@gmail.com>
2026-02-06 15:30:23 +0700

    Create HinhVe.java

---
+++ HelloWorld/src/HinhVe.java
@@ -0,0 +1,13 @@
+public class HinhVe {
+
+	public static void main(String[] args) {
+		System.out.println("   *   ");
+		System.out.println("  ***  ");
+		System.out.println(" ***** ");
+		System.out.println("*******");
+		System.out.println("   *   ");
+		System.out.println("   *   ");
+
+	}
+
+}

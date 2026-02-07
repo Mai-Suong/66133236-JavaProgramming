268489bd471d0cd6530082791467820d56d415f1
Mai-Suong <suongcudai26@gmail.com>
2026-02-07 15:33:22 +0700

    BTVN

---
+++ Bai3GiaiPhuongTrinh/src/phuongtrinhbacMot.java
@@ -0,0 +1,12 @@
+public class phuongtrinhbacMot {
+
+	public static void main(String[] args) {
+		double a = 5;
+		double b = 2;
+		double x = -b / a;
+		
+		System.out.println("Phuong trinh bac mot: " + "ax+b=0");
+		System.out.println("Nghiem cua x la: " + x);
+	}
+
+}

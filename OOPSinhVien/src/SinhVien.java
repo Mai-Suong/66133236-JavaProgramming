7c6e46a77a1c1856435c5d191374d9bceef44454
Mai-Suong <suongcudai26@gmail.com>
2026-03-05 23:00:43 +0700

    chương trình chính chưa có dl

---
+++ OOPSinhVien/src/SinhVien.java
@@ -0,0 +1,10 @@
+
+public class SinhVien {
+	private String ten;
+	private String mssv;
+	private int tuoi;
+	
+	void hocBai(String monhoc){
+		System.out.print("Bạn " + ten + " đang học môn " + monhoc);
+	}
+}

b7a1edfe527af372eb2ea67b748ab914ffecab09
Mai-Suong <suongcudai26@gmail.com>
2026-04-22 22:28:18 +0700

    thử eclipse

--- Hello/src/msuong/Hello.java
+++ Hello/src/msuong/Hello.java
@@ -1,10 +1,15 @@
 package msuong;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner bp = new Scanner(System.in);
+		
+		
 	}
 
 }

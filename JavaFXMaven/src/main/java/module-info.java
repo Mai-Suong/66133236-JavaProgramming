d0481e8c396da8e23ebd894f790c6cb796bdc458
Mai-Suong <suongcudai26@gmail.com>
2026-05-29 16:11:38 +0700

    FX Maven Single

---
+++ JavaFXMaven/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module suong.stm.JavaFXMaven {
+    requires javafx.controls;
+    exports suong.stm.JavaFXMaven;
+}
---
+++ JavaFXMaven/src/main/java/suong/stm/JavaFXMaven/SystemInfo.java
@@ -0,0 +1,13 @@
+package suong.stm.JavaFXMaven;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
